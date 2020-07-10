f05bbfc442bfe9232a1bf239da032b1a643b85b0
Nicolas Muñoz <nicolasmunoz.ocm@gmail.com>
2020-07-10 14:17:42 -0500

    Basic configuration implemented

--- src/main/java/com/niko/organizer/controller/UserController.java
+++ src/main/java/com/niko/organizer/controller/UserController.java
@@ -1,6 +1,26 @@
 package com.niko.organizer.controller;
 
 
+import com.niko.organizer.model.payload.UserSummary;
+import com.niko.organizer.response.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/me")
+    public ResponseEntity<BaseResponse<UserSummary>> getCurrentUser(){
+        return new ResponseEntity<>(BaseResponse.success(null), HttpStatus.OK);
+    }
 }
--- src/main/java/com/niko/organizer/response/BaseResponse.java
+++ src/main/java/com/niko/organizer/response/BaseResponse.java
@@ -5,12 +5,16 @@ import lombok.Data;
 
 @Builder
 @Data
-public class BaseResponse {
+public class BaseResponse <T>{
     private ResponseCode code;
     private Object data;
 
-    public static <T> BaseResponse error(final ResponseCode code){
-        return null;
+    public static <T> BaseResponse<T> error(final ResponseCode code){
+        return BaseResponse.<T>builder().code(code).data(null).build();
+    }
+    
+    public static <T> BaseResponse<T> success(final T data){
+        return BaseResponse.<T>builder().code(ResponseCode.SUCCESSFUL).data(data).build();
     }
 
 }
